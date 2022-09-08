d09fd011486d9c7b9de53b08292a78bfeebac0c4
Andreas Kvist <andreaskv@elkjop.no>
2022-09-08 23:33:31 +0200

    Added empty API project

---
+++ server/ThingyThings.Server.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThingyThings.Server.Api.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
